0c7eb7d1309b5be120c3577f37d6cac1936afe4f
Hiwot Belay Kebede <117976961+Hiwot2127@users.noreply.github.com>
2024-05-05 07:09:28 +0300

    Update Home.php

--- App/Pages/Home.php
+++ App/Pages/Home.php
@@ -11,4 +11,12 @@
         <p>Start learning today!</p>
     </div>
 </header>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Courses</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
 <body>
