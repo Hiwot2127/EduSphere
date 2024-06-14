2ad29d9e21a0d49f81c08fcdfa06206ad06fdeb8
Ililli Juhar <124351481+Lilly-jhr@users.noreply.github.com>
2024-06-14 05:53:41 +0300

    Update stripe.js

--- Backend/controllers/stripe.js
+++ Backend/controllers/stripe.js
@@ -1,27 +1,28 @@
 import express from 'express';
 import config from "../config/config.js";
 import Stripe from 'stripe';
-
 import { BaseResponse } from '../helper/baseresponse.js';
 
 const stripe = new Stripe(config.STRIPE_SECRET_KEY);
 
-export const stripePayment = async (req, res,next) => {
-    const { amount } = req.body;
+// Function to handle Stripe payment
+export const stripePayment = async (req, res, next) => {
+    const { amount } = req.body;  
   
     try {
-      const paymentIntent = await stripe.paymentIntents.create({
-        amount,
-        currency: 'usd',
-      });
-      let baseresponse = new BaseResponse();
+        const paymentIntent = await stripe.paymentIntents.create({
+            amount,
+            currency: 'usd',
+        });
+
+        let baseresponse = new BaseResponse();
         baseresponse.data = {
-        paymentIntent: paymentIntent.client_secret
-      }
-        baseresponse.success = true;
-        baseresponse.message = "Payment intent created successfully";
-        res.status(200).json({...baseresponse});
+            paymentIntent: paymentIntent.client_secret  
+        };
+        baseresponse.success = true;  
+        baseresponse.message = "Payment intent created successfully";  
+        res.status(200).json({ ...baseresponse });
     } catch (error) {
         next(error);
     }
-  }
+};
