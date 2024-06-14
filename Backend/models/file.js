9d73eb063c7a039fc6141d43d2bfed8b5cea27e5
HYT34 <124575355+HYT34@users.noreply.github.com>
2024-06-13 18:21:35 -0700

    Update file.js

--- Backend/models/file.js
+++ Backend/models/file.js
@@ -1,20 +1,22 @@
 import mongoose from "mongoose";
 
+// Define the schema for the File model
 const fileSchema = new mongoose.Schema(
     {
-        title: { type: String, required: true },
-        courseName: { type: String, required: true },
-        secureUrl: { type: String, required: true },
+        title: { type: String, required: true },  
+        courseName: { type: String, required: true },  // Name of the course associated with the file, required field
+        secureUrl: { type: String, required: true }, 
         public_id: { type: String, default: "" },
-        resource_type: { type: String, required: true },
-        size: { type: Number, default: "" },
+        resource_type: { type: String, required: true },  // Type of the resource (e.g., image, video), required field
+        size: { type: Number, default: 0 },  
         thumbnailUrl: { type: String, default: "" }
     },
     {
-        versionKey: false,
-        timestamps: true,
+        versionKey: false,  // Disable the version key (__v) which Mongoose uses for document versioning
+        timestamps: true,  // Automatically add createdAt and updatedAt fields to the schema
     }
-    );
+);
 
+// Create and export the File model based on the schema
 const File = mongoose.model("File", fileSchema);
 export default File;
